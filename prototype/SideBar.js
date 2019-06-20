7231cc09192cda13287301c99500a68e3d5c5934
Tobias Bouschen <tobias.bouschen@googlemail.com>
2019-06-20 15:34:19 +0200

    Add popup menu when icon is clicked

--- prototype/SideBar.js
+++ prototype/SideBar.js
@@ -3,6 +3,8 @@ class SideBar {
         //this.iconList = iconList;
         this.backgroundColor = backgroundColor;
 
+        this.selected = -1;
+
     }
     setDetailedView(detailedView){
         this.detailedView = detailedView;
@@ -24,17 +26,59 @@ class SideBar {
 
     }
     clicked(x,y,middle) {
-      if (y < middle-70 ){
-        console.log("sidebar plus",y);
+      //TODO introduce real views
+
+      if (y < middle - 70) {
+        if (this.selected === 0) {
+          this.detailedView.popUp = null;
+          this.selected = -1;
+
+          return;
+        }
+
+        this.selected = 0;
+
+        console.log("sidebar plus", y);
+        this.detailedView.popUp = new SidebarPopup(50, 10, this.detailedView);
       }
-      if(y < middle-40 &&y >= middle-70){
-        console.log("sidebar comment",y);
+      if (y < middle - 40 && y >= middle - 70) {
+        if (this.selected === 1) {
+          this.detailedView.popUp = null;
+          this.selected = -1;
+
+          return;
+        }
+
+        this.selected = 1;
+
+        console.log("sidebar comment", y);
+        this.detailedView.popUp = new SidebarPopup(50, 20, this.detailedView);
       }
-      if(y < middle+10 &&y >= middle-40){
-        console.log("sidebar attach",y);
+      if (y < middle + 10 && y >= middle - 40) {
+        if (this.selected === 2) {
+          this.detailedView.popUp = null;
+          this.selected = -1;
+
+          return;
+        }
+
+        this.selected = 2;
+
+        console.log("sidebar attach", y);
+        this.detailedView.popUp = new SidebarPopup(50, 30, this.detailedView);
       }
-      if(y > middle+70){
-        console.log("sidebar trash",y);
+      if (y > middle + 70) {
+        if (this.selected === 3) {
+          this.detailedView.popUp = null;
+          this.selected = -1;
+
+          return;
+        }
+
+        this.selected = 3;
+
+        console.log("sidebar trash", y);
+        this.detailedView.popUp = new SidebarPopup(50, 40, this.detailedView);
       }
     }
     // xCoor, yCoor of Node, sizeX&Y of DetailView(with frame), innerBuffer: FrameWidth
