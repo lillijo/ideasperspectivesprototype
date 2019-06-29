4482724e0d697b6fee2bcd5d3975923bd6cdfb8e
Arbeit <Arbeit>
2019-06-29 13:23:30 +0200

    loeschen funktion hinzugefügt

--- prototype/Content.js
+++ prototype/Content.js
@@ -2,6 +2,11 @@ class Content {
   constructor(title, text) {
     this.title = title;
     this.text = text;
+    this.adultShow = true;
+    this.officeWorkerShow=true;
+    this.selfOptimizerShow=true;
+    this.officeCategoryShow = true;
+    this.gamerShow = true;
 
   }
 
@@ -25,43 +30,67 @@ class Content {
       push();
       rectMode(CORNER);
       fill(ROT);
-      var firstrect = rect(xCoor - (sizeX - 30), yCoor - (sizeY - 100), 80, 30, 20);
-      //rect(xCoor-(sizeX-30), yCoor-(sizeY-130),80,30, 20);
-      var scndrect = rect(xCoor - (sizeX - 120), yCoor - (sizeY - 100), 100, 30, 20);
-      var thdrect = rect(xCoor - 70, yCoor - (sizeY - 100), 120, 30, 20);
-      //
-      // rect(xCoor-(sizeX-300), yCoor-(sizeY-90),80,30, 20);
-      fill(0);
-      text("adult", xCoor - (sizeX - 50), yCoor - (sizeY - 110));
-      text("office-worker", xCoor - (sizeX - 140), yCoor - (sizeY - 110));
-      text("self-optimizer", xCoor - 50, yCoor - (sizeY - 110));
+
+
+      if(this.adultShow){
+        var firstrect = rect(xCoor - (sizeX - 30), yCoor - (sizeY - 100), 80, 30, 20);
+        fill(0);
+        text("adult", xCoor - (sizeX - 50), yCoor - (sizeY - 110));
+      }
+      if (this.officeWorkerShow){
+        fill(ROT);
+        var scndrect = rect(xCoor - (sizeX - 120), yCoor - (sizeY - 100), 100, 30, 20);
+        fill(0);
+        text("office-worker", xCoor - (sizeX - 140), yCoor - (sizeY - 110));
+      }
+      if (this.selfOptimizerShow){
+        fill(ROT);
+        var thdrect = rect(xCoor - 70, yCoor - (sizeY - 100), 120, 30, 20);
+        fill(0);
+        text("self-optimizer", xCoor - 50, yCoor - (sizeY - 110));
+      }
       if (trashBinState == 1){
         // adult
-        image(cross, xCoor - sizeX + 32, yCoor - sizeY + 105,20,20);
+        if(this.adultShow) {
+          image(cross, xCoor - sizeX + 32, yCoor - sizeY + 105, 20, 20);
+        }
         // office worker
-        image(cross, xCoor - sizeX + 122, yCoor - sizeY + 105,20,20);
+        if (this.officeWorkerShow) {
+          image(cross, xCoor - sizeX + 122, yCoor - sizeY + 105, 20, 20);
+        }
         // self optimizer
-        image(cross, xCoor - 68, yCoor - sizeY + 105,20,20);
+        if (this.selfOptimizerShow) {
+          image(cross, xCoor - 68, yCoor - sizeY + 105, 20, 20);
+        }
       }
       pop();
     } else if (this.title == "Categories") {
       push();
       rectMode(CORNER);
       fill(GRUEN);
-      var firstrect = rect(xCoor - (sizeX - 30), yCoor - (sizeY - 90), 80, 30, 20);
-      //rect(xCoor-(sizeX-30), yCoor-(sizeY-130),80,30, 20);
-      var scndrect = rect(xCoor - (sizeX - 120), yCoor - (sizeY - 90), 100, 30, 20);
-      // rect(xCoor-(sizeX-120), yCoor-(sizeY-130),80,30, 20);
+      if (this.officeCategoryShow){
+        var firstrect = rect(xCoor - sizeX + 30, yCoor - sizeY + 90, 80, 30, 20);
+        fill(0);
+        text("office", xCoor - sizeX + 50, yCoor - sizeY + 100);
+      }
+      if (this.gamerShow){
+        fill(GRUEN);
+        var scndrect = rect(xCoor - sizeX + 120, yCoor - sizeY + 90, 100, 30, 20);
+        fill(0);
+        text("gaming", xCoor - sizeX + 140, yCoor - sizeY + 100);
+      }
       // rect(xCoor-(sizeX-210), yCoor-(sizeY-90),80,30, 20);
       // rect(xCoor-(sizeX-300), yCoor-(sizeY-90),80,30, 20);
-      fill(0);
-      text("office", xCoor - sizeX + 50, yCoor - sizeY + 100);
-      text("gaming", xCoor - sizeX + 140, yCoor - sizeY + 100);
       if (trashBinState == 1) {
         // office
-        image(cross, xCoor - sizeX + 32, yCoor - sizeY + 95, 20, 20);
+        if (this.officeCategoryShow){
+          image(cross, xCoor - sizeX + 32, yCoor - sizeY + 95, 20, 20);
+        }
         // gaming
-        image(cross, xCoor - sizeX + 122, yCoor - sizeY + 95, 20, 20);
+        if(this.gamerShow){
+          image(cross, xCoor - sizeX + 122, yCoor - sizeY + 95, 20, 20);
+        }
+
       }
       pop();
     } else if (this.title == "Description") {
@@ -96,6 +125,37 @@ class Content {
   clicked(x, y) {
     if (this.closeIconIsClicked(x, y)) {
       this.parent.node.switchViews();
+    }
+    // Mülleimer funktionen
+    if (this.parent.trashBinState==1) {
+
+      if (this.title == "Target Audience") {
+        // alle CloseIcons sind auf der gleichen Höhe
+        if (this.parent.node.y - this.parent.sizeY + 95 < y && y < this.parent.node.y - this.parent.sizeY + 115) {
+          // alle Werte kommen aus this.draw() bei title == target audience
+          if (this.parent.node.x - this.parent.sizeX + 22 < x && x < this.parent.node.x - this.parent.sizeX + 42) {
+            this.adultShow = false;
+            console.log("adult close");
+          }
+          if (this.parent.node.x - this.parent.sizeX + 112 < x && x < this.parent.node.x - this.parent.sizeX + 132) {
+            this.officeWorkerShow = false;
+          }
+          if (this.parent.node.x - 78 < x && x < this.parent.node.x - 58) {
+            this.selfOptimizerShow = false;
+          }
+        }
+      }
+      if (this.title == "Categories") {
+        if (this.parent.node.y - this.parent.sizeY + 90 < y && y < this.parent.node.y - this.parent.sizeY + 110) {
+          // alle Werte kommen aus this.draw() bei title == Categories
+          if (this.parent.node.x - this.parent.sizeX + 22 < x && x < this.parent.node.x - this.parent.sizeX + 42) {
+            this.officeCategoryShow = false;
+          }
+          if (this.parent.node.x - this.parent.sizeX + 112 < x && x < this.parent.node.x - this.parent.sizeX + 132) {
+            this.gamerShow = false;
+          }
+        }
+      }
     }
   }
 
